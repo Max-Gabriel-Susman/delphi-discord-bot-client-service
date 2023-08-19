55b5da57bbdcedc257f84f713673ea7a7e1cba76
Gabe Susman <36492747+Max-Gabriel-Susman@users.noreply.github.com>
2023-08-19 08:28:28 -0600

    got communications working with inferential svc, healthcheck is working

--- cmd/delphi-discord-bot-client-service/main.go
+++ cmd/delphi-discord-bot-client-service/main.go
@@ -61,14 +61,11 @@ func run(ctx context.Context, _ []string) error {
 
 	inferenceClient := inference.NewClient("inference", "http://localhost:8080")
 
-	fmt.Println("before discord connection") // delete
 	sess, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("after discord connection") // delete
 
-	fmt.Println("before handler additon") // delete
 	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if m.Author.ID == s.State.User.ID {
 			return
@@ -89,6 +86,7 @@ func run(ctx context.Context, _ []string) error {
 
 		if args[1] == statusArg {
 			// TODO: execute healthcheck on inference service
+			fmt.Println("Healthcheck started") // delete
 			inferenceClient.HealthCheck(ctx)
 			s.ChannelMessageSend(m.ChannelID, apolloStatusOnlineResponse)
 		} else if args[1] == promptArg {
@@ -129,17 +127,14 @@ func run(ctx context.Context, _ []string) error {
 			s.ChannelMessageSend(m.ChannelID, unknownSubCommandResponse)
 		}
 	})
-	fmt.Println("after handler additon") // delete
 
 	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
 
-	fmt.Println("before session open") // delete
 	err = sess.Open()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer sess.Close()
-	fmt.Println("after session open") // delete
 
 	fmt.Println(apolloStatusOnlineResponse)
 
--- internal/clients/inference/healthcheck.go
+++ internal/clients/inference/healthcheck.go
@@ -14,7 +14,7 @@ func (c *Client) HealthCheck(ctx context.Context) (*ErrorResponse, error) {
 	// if span, ok := tracer.SpanFromContext(ctx); ok {
 	// 	span.SetTag(ext.ManualDrop, true)
 	// } TODO: add tracing
-
+	fmt.Println("Healthcheck requested") // delete
 	const path = "/health"
 
 	req, err := c.NewRequest(ctx, "GET", path, nil)
@@ -30,6 +30,6 @@ func (c *Client) HealthCheck(ctx context.Context) (*ErrorResponse, error) {
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("unexpected healthcheck status code response: %d", resp.StatusCode)
 	}
-
+	fmt.Println("Healthcheck completed") // delete
 	return nil, nil
 }
